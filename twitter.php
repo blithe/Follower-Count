f719ec7095e4a1155bbbfa82dbe9a2d0a0bc236f
Scott Rocher <scottrocher@gmail.com>
2012-02-18 23:58:16 -0500

    Update twitter.php

--- twitter.php
+++ twitter.php
@@ -1,9 +1,13 @@
 <?php
+	
+	// set this to the twitter handle you want to get follower count for
+	$twitterHandle = 'blithe';
+	
 	// initialize cURL session
 	$curl_handle=curl_init();
 	
 	// set the URL of the page to download
-	curl_setopt($curl_handle,CURLOPT_URL,'https://api.twitter.com/1/users/show.json?screen_name=blithe&include_entities=true');
+	curl_setopt($curl_handle,CURLOPT_URL,'https://api.twitter.com/1/users/show.json?screen_name=' . $twitterHandle . '&include_entities=true');
 	
 	// set the timeout to 2 seconds
 	curl_setopt($curl_handle,CURLOPT_CONNECTTIMEOUT,2);
@@ -23,4 +27,4 @@
 	
 ?>
 
-Blithe has <?php echo $array["followers_count"]; ?> followers.
+<a href="http://twitter.com/<?php echo $twitterHandle; ?>@<?php echo $twitterHandle; ?></a> has <?php echo $array["followers_count"]; ?> followers.
